cdf6e32d8ffa18aaac19573ea31ef8b3c2ea8984
xufei851105 <18755119845@163.com>
2019-03-26 20:56:10 +0800

    first commit

---
+++ src/main/java/cn/proem/xufei/XufeiApplication.java
@@ -0,0 +1,13 @@
+package cn.proem.xufei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XufeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XufeiApplication.class, args);
+    }
+
+}
